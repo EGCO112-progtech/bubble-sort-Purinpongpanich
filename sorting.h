407fd92146226089a8e18a9f9f2d440600cfe398
Purinpongpanich <123047827+Purinpongpanich@users.noreply.github.com>
2023-01-27 10:31:38 +0000

    เย้

--- sorting.h
+++ sorting.h
@@ -32,7 +32,6 @@ void bubbleSort(int a[], int n) {
   // how may pair to compare?
   for (i = 0; i < n - 1; i++) {
     sorted = 0;
-    printf("Round %d\n", i);
     for (j = 0; j < n - i - 1; j++) {
       if (a[j] > a[j + 1]) {
         swap(&a[j], &a[j + 1]);
